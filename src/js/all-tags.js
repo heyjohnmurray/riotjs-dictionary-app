d76e9a03c284437bc61a85eb7bb47f7208cac011
John Murray <heyjohnmurray@us.ibm.com>
2016-06-07 17:56:47 -0500

    figured out how to nest tags

--- src/js/all-tags.js
+++ src/js/all-tags.js
@@ -1,4 +1,10 @@
-riot.tag2('app', '<nav> <h3>Nav</h3> <ul> <li each="{color in opts.colorsList}">{color.name}</li> </ul> </nav> <main> <h1>Content</h1> </main>', '', '', function(opts) {
+riot.tag2('app', '<nav> <h3>Nav</h3> <ul> <li each="{color in opts.colorsList}">{color.name}</li> </ul> </nav> <main> <h1>Content</h1> <newtag></newTag> <testtag value="John"></testTag> </main>', '', '', function(opts) {
+});
+
+riot.tag2('newtag', '<h2>New Tag</h2> <p>Here are my friends:</p> <p> <ul> <li each="{friend in opts.peeps}">{friend.name}</li> </ul> </p>', '', '', function(opts) {
+});
+
+riot.tag2('testtag', '<h1>{opts.value}</h1>', '', '', function(opts) {
 });
 
 riot.tag2('definition-area', '<section> <h1>term</h1> <p>definition</p> </section>', '', '', function(opts) {
--- src/tags/app.tag
+++ src/tags/app.tag
@@ -7,5 +7,21 @@
   </nav>
   <main>
     <h1>Content</h1>
+    <newTag></newTag>
+    <testTag value="John"></testTag>
   </main>
 </app>
+
+<newTag>
+  <h2>New Tag</h2>
+  <p>Here are my friends:</p>
+  <p>
+    <ul>
+      <li each={friend in opts.peeps}>{friend.name}</li>
+    </ul>
+  </p>
+</newTag>
+
+<testTag>
+  <h1>{opts.value}</h1>
+</testTag>
